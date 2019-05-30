ecfcbb0abbdc6887a6ae5cf4eedc316bfe1c2f12
greghernandez <gregoryarthur13@gmail.com>
2019-05-30 13:28:46 -0500

    Brach listo

--- routes/branc.js
+++ routes/branc.js
@@ -23,6 +23,8 @@ router.get("/", (req, res, next) => {
         db.end((err) => { console.log("Closed") })
     });
 });
+
+//Selecciona el dato con el id
 router.get("/:id", (req, res, next) => {
     const db = mysql.createConnection(dbconn);
     const query = `SELECT * FROM branc WHERE id = ${req.params.id}`;
@@ -39,11 +41,10 @@ router.get("/:id", (req, res, next) => {
 });
 
 //Inserta datos
-
 router.post("/", (req, res, next) => {
     const db = mysql.createConnection(dbconn);
     const query = `INSERT INTO branc ( name, address, visitDay) 
-                   VALUES ('${req.body.name}', '${req.body.address}', '${req.body.visitDay}'s)`;
+                   VALUES ('${req.body.name}', '${req.body.address}', '${req.body.visitDay}')`;
     db.query(query, (err, result, fields) => {
         console.log(err)
         if (err) {
@@ -56,18 +57,25 @@ router.post("/", (req, res, next) => {
         db.end((err) => { console.log("closed") });
     });
 });
-//Cambios una vez creadio el campo
+
+//Cambios una vez creado el campo
 router.put("/", (req, res, next) => {
     const db = mysql.createConnection(dbconn);
     const qry = `SELECT COUNT(*) as total FROM branc WHERE id = ${req.body.id}`;
     console.log(qry);
     query = `UPDATE branc SET `;
     if(req.body.name){
-        query += `name = '${req.body.name}', `;
+        query += `name = '${req.body.name}'`;
+        if(req.body.address || req.body.visitDay){
+            query += `, `;
+        }
     }
 
     if(req.body.address){
-        query += `address = '${req.body.address}', `;
+        query += `address = '${req.body.address}' `;
+        if(req.body.visitDay){
+            query += `, `;
+        }
     }
 
     if(req.body.visitDay){
@@ -102,4 +110,20 @@ router.put("/", (req, res, next) => {
         }   
     });
 });
+
+// Eliminar un branch por id
+router.delete("/", (req,res, next) => {
+    const db = mysql.createConnection(dbconn);
+    const query = `DELETE FROM branc WHERE id = ${req.body.idBranc}`;
+    db.query(query, (err,result, fields) => {
+        if(err){
+            res.status(500);
+            res.json({code: 0, message: "Algo salió mal"})
+        }
+        res.status(200);
+        res.json({ code: 1, message: "Branch eliminado correctamente"})
+        db.end((err) => { console.log("Closed")})
+    })
+});
+
 module.exports = router;
